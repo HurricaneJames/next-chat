43f720c1f1dd94f9749b7a8e376a2cdb791dc427
James Burnett <HurricaneJamesEsq@gmail.com>
2023-10-01 18:01:55 -0700

    remove discord provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -49,10 +49,6 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     GithubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
