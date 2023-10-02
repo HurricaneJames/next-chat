02c91bbd837f5e9d413838b51dc17eeff48d56d2
James Burnett <HurricaneJamesEsq@gmail.com>
2023-10-01 21:03:31 -0700

    Added posts to landing page and made some basic style changes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
+// import Link from "next/link";
 
 import { api } from "~/utils/api";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 export default function Home() {
   return (
@@ -15,8 +19,9 @@ export default function Home() {
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Next <span className="text-[hsl(280,100%,70%)]">Chat</span> →
+            Next <span className="text-fuchsia-500">Chat</span> →
           </h1>
+          <CreatePost />
           <Posts />
           <div className="flex flex-col items-center gap-2">
             <AuthShowcase />
@@ -27,18 +32,40 @@ export default function Home() {
   );
 }
 
+function CreatePost() {
+  const { data: sessionData } = useSession();
+  const user = sessionData?.user;
+  if (!user) return null;
+
+  console.log('User', user);
+
+  return (
+    <div className="flex gap-4">
+      <img src={user.image || ""} alt="Profile image" className="w-16 h-16 rounded-full" />
+      <input placeholder="What's on your mind?" className="bg-transparent " />
+    </div>
+  )
+}
+
 function Posts() {
   const { data: posts } = api.post.getAll.useQuery();
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:gap-8">
       {posts?.map((post) => (
-        <Link key={post.id}
-          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-          href={`/posts/${post.id}`}
-          target="_blank"
+        <div
+          key={post.id}
+          className="flex max-w-md gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
         >
-          {post.content}
-        </Link>
+          <img src={post.author.image || ""} alt="Profile image" className="w-12 h-12 rounded-full" />
+          <div className="flex flex-col">
+            <div className="flex flex-row gap-2">
+              <span className="text-fuchsia-500">{`@${post.author.name}`}</span>
+              <span>{`·`}</span>
+              <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
+            </div>
+            <span>{post.content}</span>
+          </div>
+        </div>
       ))}
     </div>
   );
@@ -49,11 +76,11 @@ function AuthShowcase() {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
+      <p className="text-center text-2xl">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
       </p>
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,18 +1,28 @@
 import { z } from "zod";
 
 import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
+    createTRPCRouter,
+    protectedProcedure,
+    publicProcedure,
 } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
-  }),
+    getAll: publicProcedure.query(({ ctx }) => {
+        return ctx.db.post.findMany({
+            take: 100,
+            include: {
+                author: {
+                    select: {
+                        name: true,
+                        image: true,
+                    }
+                },
+            },
+        });
+    }),
 
-  getViewerPosts: protectedProcedure.query(({ctx}) => {
-    // TODO - return viewer posts
-    return ctx.db.post.findMany();
-  }),
+    getViewerPosts: protectedProcedure.query(({ ctx }) => {
+        // TODO - return viewer posts
+        return ctx.db.post.findMany();
+    }),
 });
