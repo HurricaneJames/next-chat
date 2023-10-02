390fd56222ded45620985ad06dc6fba8aa5a781a
James Burnett <HurricaneJamesEsq@gmail.com>
2023-10-01 22:05:53 -0700

    Added rate limiting via upstash.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 dayjs.extend(relativeTime);
 
 const LOGO = '→';
@@ -37,22 +38,32 @@ export default function Home() {
 }
 
 function CreatePost() {
-  const { data: sessionData } = useSession();
-  const user = sessionData?.user;
-  if (!user) return null;
-
   const ctx = api.useContext();
   const { mutate, isLoading: isMutating } = api.post.create.useMutation({
     onSuccess: () => {
       setInput("");
       void ctx.post.invalidate();
+    },
+    onError: (error) => {
+      const contentError = error.data?.zodError?.fieldErrors.content;
+      if (contentError && contentError.length > 0) {
+        return toast.error(contentError[0] || "");
+      }
+      if (error.message == "TOO_MANY_REQUESTS") {
+        return toast.error("Too many posts. Please wait a while and try again.");
+      }
+
+      return toast.error("Something went wrong. Please try again later");
     }
   });
   const [input, setInput] = useState("");
 
+  const { data: sessionData } = useSession();
+  const user = sessionData?.user;
+  if (!user) return null;
 
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-4 flex-row">
       <img src={user.image || ""} alt="Profile image" className="w-16 h-16 rounded-full" />
       <input
         placeholder="What's on your mind?"
@@ -60,12 +71,25 @@ function CreatePost() {
         value={input}
         onChange={e => setInput(e.target.value)}
         disabled={isMutating}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
       />
-      <button onClick={() => {
-        if (!isMutating) {
+      {isMutating && <div className="flex justify-center items-center"><LoadingSpinner /></div>}
+      {!isMutating && <button
+        className={(input.length < 1) ? "opacity-50" : ""}
+        disabled={input.length < 1}
+        onClick={() => {
           mutate({ content: input });
-        }
-      }}>{LOGO}</button>
+        }}>
+          {LOGO}
+        </button>
+      }
     </div>
   )
 }
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,3 +1,6 @@
+import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 import { z } from "zod";
 
 import {
@@ -6,13 +9,21 @@ import {
     publicProcedure,
 } from "~/server/api/trpc";
 
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    // limit to 3 posts in a 1 minute sliding window
+    limiter: Ratelimit.slidingWindow(3, "1 m"),
+    analytics: true,
+    prefix: "next-chat-studio",
+});
+
 export const postRouter = createTRPCRouter({
     getAll: publicProcedure.query(({ ctx }) => {
         return ctx.db.post.findMany({
             take: 100,
             orderBy: [
                 { createdAt: 'desc' },
-            ], 
+            ],
             include: {
                 author: {
                     select: {
@@ -28,8 +39,12 @@ export const postRouter = createTRPCRouter({
         z.object({
             content: z.string().min(1).max(1500),
         })
-    ).mutation(async ({ctx, input}) => {
+    ).mutation(async ({ ctx, input }) => {
         const authorId = ctx.session.user.id;
+
+        const { success } = await ratelimit.limit(authorId)
+        if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
         const post = await ctx.db.post.create({
             data: {
                 authorId,
